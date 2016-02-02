71beb51e644f746f72720b0638639f30823b4f87
Daniel Jeller <djeller@nettek.at>
2016-02-02 14:22:52 +0100

    Toggle bibltenor visibility in sync with tenor

--- my/XRX/src/mom/app/charter/js/charter.js
+++ my/XRX/src/mom/app/charter/js/charter.js
@@ -5,20 +5,20 @@
 // });
 
 
-function changeImage(url, num){	
-	var alleBilder = document.getElementsByClassName('imageLink');	
+function changeImage(url, num){
+	var alleBilder = document.getElementsByClassName('imageLink');
 	for (var i=0;i<alleBilder.length; i++){
-		
+
 		if(alleBilder[i].text == num ){
 			alleBilder[i].setAttribute('id', 'gelb');
 		}
 		else {
 			alleBilder[i].setAttribute('id', 'black');
 		}
-		
-	}	
+
+	}
 	document.images["image"].src = url;
-	document.getElementById('img-link').setAttribute('href', url);	
+	document.getElementById('img-link').setAttribute('href', url);
   // reload SVG and cancel create- process
   //jQuery(document).imageTool.resetSVGId();
   //jQuery(document).imageTool.loadSVG();
@@ -35,66 +35,72 @@ function changeGraphic(wit, url)
 
 function showHideDiv(id, triangle)
 {
-	
+
 	if(document.getElementById(id).style.display == "none")
 	{
-			
-		document.getElementById(id).style.display = "block";	
-		        
+
+		document.getElementById(id).style.display = "block";
+
 	}
 	else
-	{	
+	{
 		document.getElementById(id).style.display = "none";
-		
+
 	}
 }
 
 function showHideDiv_neu(id, triangle)
 {
-	
+
 	var xmlid = '#'+ id;
-		$(xmlid).toggle( function(){		
-		
+		$(xmlid).toggle( function(){
+
 			if ($(this)[0].style.display == 'none'){
-				var bild = $('div#Icons').find('img');				
-				var b = $(bild)[1];				
-				$(b).css("height", "20px");				
+				var bild = $('div#Icons').find('img');
+				var b = $(bild)[1];
+				$(b).css("height", "20px");
 				var cIcon2 = $(b).clone();
-				var img2 = $(cIcon2)[0];				
+				var img2 = $(cIcon2)[0];
 				var icontillnow = $(xmlid).prev("div.cat").find("img#aus");
-				$(icontillnow[0]).replaceWith(img2);				
-				
+				$(icontillnow[0]).replaceWith(img2);
+
 			}
 			else {
 				var bild = $('div#Icons').find('img');
-				var a = $(bild)[0];				
-				$(a).css("height", "20px");				
-				var cIcon1 = $(a).clone();				
-				var img1 = $(cIcon1)[0];				
+				var a = $(bild)[0];
+				$(a).css("height", "20px");
+				var cIcon1 = $(a).clone();
+				var img1 = $(cIcon1)[0];
 				var icontillnow = $(xmlid).prev("div.cat").find("img#ein");
-				$(icontillnow[0]).replaceWith(img1);				
+				$(icontillnow[0]).replaceWith(img1);
 			}
-		
+
 		} );
+
+	// bibltenor belongs to tenor and has to be toggled in sync
+	if(id=="tenor"){
+		$('#bibltenor').toggle();
+	}
+
 }
 
 
 function hideDiv(id)
-{	
+{
 	document.getElementById(id).style.display = "none";
 }
 
 
 
 function showDiv(id)
-{	
+{
 	document.getElementById(id).style.display = "block";
 }
 
 
 
 function assignDiv(hide, show)
-{	
+{
 	var divs = new Array();
 	divs = hide.split(' ');
 	for (n = 0; n < divs.length; n++)
@@ -112,7 +118,7 @@ function assignDiv(hide, show)
 
 
 function showContent(hide, show)
-{	
+{
 	var divs = new Array();
 	divs = hide.split(' ');
 	for (n = 0; n < divs.length; n++)
@@ -129,7 +135,7 @@ function proof(){
 var bild = $('div#Icons').find('img');
 
 var a = $(bild)[0];
-var b = $(bild)[1];	
+var b = $(bild)[1];
 $(a).css("height", "20px");
 $(b).css("height", "20px");
  clonedIcon1 = $(a).clone();
@@ -144,24 +150,24 @@ var allNone = $("div[style='display:none']").prev('div.cat').children().append(c
 var show = $("div.cat").next(":not(div[style='display:none'])");
 var allShown = $(show).prev('div.cat').children().append(clonedIcon1);
 /* need to define exceptions because of irregularities in the html structure!!!!!*/
-if($("div#abstract").children().children().length == 0){	
+if($("div#abstract").children().children().length == 0){
 	$("div.abs").find('img').replaceWith(clon2);
 }
 
-if($("div#indexlist").children().length == 0) {	
+if($("div#indexlist").children().length == 0) {
 	$("div.idx").find('img').replaceWith(clone2);
-	
+
 }
 else {
-		
+
 	$("div.idx").find('img').replaceWith(clone1);
 }
-if($("div.no-graphic").length >= 1) {	
-	
+if($("div.no-graphic").length >= 1) {
+
 	$("div#graphicheader").children().append(cloneI2);
-	$("div#graphicheader").nextAll("img").remove();	
+	$("div#graphicheader").nextAll("img").remove();
 }
-else 	{	
+else 	{
 	$("div#graphicheader").children().append(cloneI1);
 	$("div#graphicheader").nextAll("img").remove();
 	}
