d4849d933c0f66784130556c35e619209aedd148
Jan <JBigalke@smail.uni-koeln.de>
2018-01-25 13:14:54 +0100

    jquery functions for dialogs.

--- my/XRX/src/mom/app/charter/js/jquery.ui.dialogCopyToCollection.js
+++ my/XRX/src/mom/app/charter/js/jquery.ui.dialogCopyToCollection.js
@@ -1,18 +1,19 @@
 
-function createLegalDialog(requestRoot, userid, charterid){
+function createLegalDialog(requestRoot, charterid, owner){
+		$("#insertOwner").append(owner);
 	    $("#copy-legal-popUp").dialog({
 			      				width: 500,
 			      				buttons:{
 									Cancel: function(){$(this).dialog("close");},
 									"Accept": function(){
-										callserviceMyCollectionsTree(requestRoot, charterid ,charterid);
+										callserviceMyCollectionsTree(requestRoot, charterid, owner);
 										$(this).dialog("close");
 									}
 			      				}
 	    	});
 };
 
-function callserviceMyCollectionsTree(requestRoot, userid, charterid){	
+function callserviceMyCollectionsTree(requestRoot, charterid, owner){	
 	var MyCollectionsTree = "my-collections-tree";
 	$.ajax({
 		url: createServiceUrl(requestRoot, MyCollectionsTree),
@@ -23,19 +24,19 @@ function callserviceMyCollectionsTree(requestRoot, userid, charterid){
 	        	var option = "<option value='"+key+"'>"+value.title+"</option>";
 	        	$("#collectionSelect").append(option);
 	        });
-			createSelectDialog(requestRoot, userid, charterid);
+			createSelectDialog(requestRoot, charterid, owner);
 		}
 	});
 };
 
-function createSelectDialog(requestRoot, userid, charterid) {
+function createSelectDialog(requestRoot, charterid, owner) {
 	$('#select-collection-popUp').dialog({
 	      width: 500,
 			buttons:{
 				Cancel: function(){$(this).dialog("close");},
 				"Accept": function(){
 					$(this).dialog("close");
-					callserviceMyCollectionCopyCharter(requestRoot, userid, charterid);
+					callserviceMyCollectionCopyCharter(requestRoot, charterid, owner);
 				}
 			}
 	});
@@ -43,14 +44,14 @@ function createSelectDialog(requestRoot, userid, charterid) {
 
 function createServiceUrl(requestRoot, service, charterid){ return requestRoot +"service/" +service};
 
-function callserviceMyCollectionCopyCharter(requestRoot, userid, charterid){
+function callserviceMyCollectionCopyCharter(requestRoot, charterid, owner){
 	var MyCollectionCopyCharter = "my-collection-copy-charter";
 	var selected_collection = $("#collectionSelect").val();
 	console.log("charterid" +charterid);
 	$.ajax({
 		url: createServiceUrl(requestRoot, MyCollectionCopyCharter),
 		type: "POST",
-		data: {collection: selected_collection, charterid: charterid},
+		data: {collection: selected_collection, charterid: charterid, owner: owner},
 		dataType: "text",
 		success: function(data, textStatus, jqXHR){
 			console.log(data);
@@ -58,79 +59,3 @@ function callserviceMyCollectionCopyCharter(requestRoot, userid, charterid){
 	});
 };
 
-$.widget( "ui.dialogCopyToCollection", {
-		   /*
-			options: {
-				
-		
-				requestRoot: "",
-				userid: "",
-				charterid: "",
-				
-			},
-			*/
-			//_serviceUrl: function(service){ return this.options.requestRoot + service; },
-		    
-			_create: function() {
-				console.log("abfuck number 3");
-			    //console.log(this);
-				/*
-				var self = this;
-				this.legalDialog = $("#copy-legal-popUp");
-				this.selectCollectionDialog = $('#select-collection-popUp');
-				//this.Button = $("#copycollection").click(function(){self._createlegalDialog(self)});
-				this.serviceMyCollectionsTree = "service/my-collections-tree";
-				this.serviceMyCollectionCopyCharter = "service/my-collection-copy-charter";
-			    */
-			}
-			/*
-			
-			_createlegalDialog: function(self){
-				this.legalDialog.show();
-				this.legalDialog.dialog({
-				      modal: true,
-				      width: 500,
-						buttons:{
-							Cancel: function(){$(this).dialog("close");},
-							"Accept": function(){
-								//self.callserviceMyCollectionsTree(self);
-								$(this).dialog("close");
-							}
-							}
-					
-				});
-			}
-			
-			createSelectDialog: function(self){
-				this.selectCollectionDialog.show();
-				this.selectCollectionDialog.dialog({
-				      modal: true,
-				      width: 500,
-						buttons:{
-							Cancel: function(){$(this).dialog("close");},
-							"Accept": function(){
-								$(this).dialog("close");
-								//self.callserviceMyCollectionCopyCharter();
-							}
-							}
-				});
-			},
-			
-			callserviceMyCollectionCopyCharter: function(){
-				var selected_collection = $("#collectionSelect").val();
-				$.ajax({
-					url: this._serviceUrl(this.serviceMyCollectionCopyCharter),
-					type: "POST",
-					data: {collection: selected_collection, charterid: this.options.charterid, context: this.options.context},
-					dataType: "text",
-					success: function(data, textStatus, jqXHR){
-						console.log(data);
-					}
-				});
-			},
-			
-			
-
-			*/
-			
-		});
