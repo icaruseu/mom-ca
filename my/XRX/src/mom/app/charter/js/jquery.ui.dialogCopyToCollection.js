0c39d667ed52b0a7ba1815fd8a2b9dcb1c896652
Jan <JBigalke@smail.uni-koeln.de>
2018-02-09 02:43:30 +0100

    added function to clean selectMenu

--- my/XRX/src/mom/app/charter/js/jquery.ui.dialogCopyToCollection.js
+++ my/XRX/src/mom/app/charter/js/jquery.ui.dialogCopyToCollection.js
@@ -20,26 +20,56 @@ function callserviceMyCollectionsTree(requestRoot, charterid, owner){
 		type: "GET",
 		dataType: "json",
 		success: function(data, textStatus, jqXHR){
-	        $.each(data, function(key, value) { 
+			$("#collectionSelect").empty();
+			var defaultOption = "<option>Select Collection</option>";
+			$("#collectionSelect").append(defaultOption);
+	        $.each(data, function(key, value) {	        
 	        	var option = "<option value='"+key+"'>"+value.title+"</option>";
 	        	$("#collectionSelect").append(option);
+	        	$("#collectionSelect").change(function(){
+	        		callserviceMyCollectionCheckCharterExists(requestRoot, charterid)
+	        	})
 	        });
 			createSelectDialog(requestRoot, charterid, owner);
 		}
 	});
 };
 
+
+function callserviceMyCollectionCheckCharterExists(requestRoot, charterid){
+	var MyCollectionCheckCharterExists = "my-collection-check-charter-exists";
+	var selected_collection = $("#collectionSelect").val();
+	$.ajax({
+		url: createServiceUrl(requestRoot, MyCollectionCheckCharterExists),
+		type: "POST",
+		data: {collection: selected_collection, charterid: charterid},
+		dataType: "text",
+		success: function(data, textStatus, jqXHR){
+			if(data == "true"){$("#Copy-Charter-Button").button('disable');}
+			else if(data == "false"){$("#Copy-Charter-Button").button('enable');}
+		}
+	});
+}
+
 function createSelectDialog(requestRoot, charterid, owner) {
 	$('#select-collection-popUp').dialog({
 	      width: 500,
 			buttons:{
-				Cancel: function(){$(this).dialog("close");},
-				"Accept": function(){
-					$(this).dialog("close");
+				"Cancel": { text:"Cancel",
+						    id: "Cancel-Button",
+						    click: function(){$(this).dialog("close");}
+				},
+				"CopyCharter": {
+					text: "Copy Charter",
+					id: "Copy-Charter-Button",
+					click: function(){
+				    $(this).dialog("close");
 					callserviceMyCollectionCopyCharter(requestRoot, charterid, owner);
 				}
+			  }
 			}
 	});
+	$("#Copy-Charter-Button").button('disable');
 };
 
 function createServiceUrl(requestRoot, service, charterid){ return requestRoot +"service/" +service};
@@ -47,7 +77,6 @@ function createServiceUrl(requestRoot, service, charterid){ return requestRoot +
 function callserviceMyCollectionCopyCharter(requestRoot, charterid, owner){
 	var MyCollectionCopyCharter = "my-collection-copy-charter";
 	var selected_collection = $("#collectionSelect").val();
-	console.log("charterid" +charterid);
 	$.ajax({
 		url: createServiceUrl(requestRoot, MyCollectionCopyCharter),
 		type: "POST",
