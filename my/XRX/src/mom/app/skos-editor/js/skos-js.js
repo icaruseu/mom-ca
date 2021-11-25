0a72aaa6665ee4c1670f4d7329c568712e37f894
NTsch <niklas.tscherne@protonmail.com>
2021-11-25 13:28:03 +0100

    experimenting with service-based approach with ajax

--- my/XRX/src/mom/app/skos-editor/js/skos-js.js
+++ my/XRX/src/mom/app/skos-editor/js/skos-js.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide();
-  });
+var editor = CodeMirror.fromTextArea
+(document.getElementById('editor'), {
+    mode: 'xml',
+    theme: 'cobalt',
+    lineWrapping: true,
+    indentUnit: 4
 });
